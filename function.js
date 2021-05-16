4860f27f3284afb1197aa60b475a545450c3e976
Le Tran Trong Hung <s3805504@rmit.edu.vn>
2021-05-16 10:56:51 +0700

    Enter

--- function.js
+++ function.js
@@ -13,29 +13,44 @@ function showSlides() {
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 
-
+// Reserve Modal
 // Get the modal
-var modal = document.getElementById("myModal");
-
+var modalReserve = document.getElementById("reserveModal");
 // Get the button that opens the modal
-var btn = document.getElementById("reserveLink");
-
+var btnReserve = document.getElementById("reserveLink");
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
+var spanReserve = document.getElementById("reserveClose");
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btnReserve.onclick = function() {
+  modalReserve.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+spanReserve.onclick = function() {
+  modalReserve.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalReserve) {
+    modalReserve.style.display = "none";
   }
-} 
+}
+
+// Sign In Modal
+var modalSignin = document.getElementById("signinModal");
+var btnSignin = document.getElementById("signinLink");
+// Get the <span> element that closes the modal
+var spanSignin = document.getElementById("signinClose");
+// When the user clicks on the button, open the modal
+btnSignin.onclick = function() {
+  modalSignin.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+spanSignin.onclick = function() {
+  modalSignin.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalSignin) {
+    modalSignin.style.display = "none";
+  }
+}
